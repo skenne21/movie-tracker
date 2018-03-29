fdbdb5e83e2e4988b44e72dea29ab5dd7c61bb09
Kailin Cannon <kailin.cannon@gmail.com>
2018-03-28 18:03:22 -0600

    Connecting moviewrapper through moviecontainer to redux store

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import SignIn from '../SignIn/SignIn';
 import { Switch, Route } from 'react-router-dom';
-import MovieContainer from '../../components/MovieContainer/MovieContainer'
+import MovieWrapper from '../../containers/movieContainer/movieContainer';
 import * as movieData from '../../helper/apiCall';
 
 export class App extends Component {
-  
+
   fetchMovies = async () => {
     const movies = await movieData.fetchRecentMovies();
     this.props.fetchMovies(movies)
@@ -15,13 +15,13 @@ export class App extends Component {
   componentDidMount() {
     this.fetchMovies();
   }
-  
+
   render() {
     return (
       <div className="App">
         <header>MovieTracker</header>
         <Switch>
-          <Route exact  path="/" component={MovieContainer}/>
+          <Route exact  path="/" component={MovieWrapper}/>
           <Route path="/login" component={SignIn} />
         </Switch>
       </div>
@@ -30,4 +30,3 @@ export class App extends Component {
 }
 
 export default App;
-
---
+++ src/containers/movieContainer/movieContainer.js
@@ -0,0 +1,14 @@
+import MovieWrapper from '../../components/MovieWrapper/MovieWrapper';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+export const mapStateToProps = state => ({
+  movies: state.movies
+});
+
+export const mapDispatchToProps = dispatch => ({
+  fetchMovies: (movies) => dispatch(actions.loadMovies(movies))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieWrapper);
