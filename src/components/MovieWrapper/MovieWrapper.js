b71134422a0a4005e10bc7263d21350f72ffc2ac
Sabrina Kennedy <skenne21@gmail.com>
2018-03-31 13:28:29 -0600

    Fixing router path issues

--- src/components/MovieWrapper/MovieWrapper.js
+++ src/components/MovieWrapper/MovieWrapper.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Movie from '../Movie/Movie';
 import { getFavorites } from '../../helper/apiCall';
 import PropTypes from 'prop-types';
 
-const MovieWrapper = (props) =>  {
-  const pathName = props.history.location.pathname;
+class MovieWrapper extends Component  {
+  constructor(props) {
+    super(props)
   
-  const createMovie = props.movies.map(movie =>
-    <Movie
-      key={movie.id}
-      movie={movie}
-      user={props.user}
-      addFavorites={props.addFavorites}
-      handleUser={props.handleUser}
-    />
-  );
+  }
 
-  return (
-    <div>
-      {createMovie}
-    </div>
-  );
-};
+  
+
+  componentDidMount() {
+    this.determinePath()
+  }
+  
+
+  determinePath = () => {
+    if(this.props.user.length) {
+
+      switch(this.props.history.location.pathname) {
+      case "/":
+        return this.createMovie(this.props.movies)
+      case "/favorites":
+        return this.createMovie(this.props.user[0].favorites)
+      default:
+        return null;
+      }
+    }
+    else {
+      this.props.history.push('/login')
+      return (
+        <div></div>
+      )
+    }
+    
+  }
+  
+  createMovie = (movies) => {
+    const mappedMovies = movies.map(movie => {
+      return <Movie
+        key={movie.id}
+        movie={movie}
+        user={this.props.user}
+        addFavorites={this.props.addFavorites}
+        handleUser={this.props.handleUser}
+      />
+    });
+
+    return (
+      <div className='movie'>
+        {mappedMovies}
+      </div>
+    )
+  }
+  
+  render() {
+    return (
+      this.determinePath()
+    )
+  }
+}
 
 MovieWrapper.propTypes = {
   movies: PropTypes.array,
