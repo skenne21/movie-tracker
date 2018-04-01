8abb457e9e3c3c48fbcdb1cccd9125532647f043
Sabrina Kennedy <skenne21@gmail.com>
2018-04-01 17:31:37 -0600

    Add reducers tests

---
+++ src/reducers/moviesReducers.test.js
@@ -0,0 +1,16 @@
+import { moviesReducer } from './moviesReducer';
+import * as actions from '../actions/index';
+import { cleanData } from '../mocks/mockMovieData';
+
+describe('moviesReducers', () => {
+
+  it('Should return a default state', () => {
+    const expected = [];
+    expect(moviesReducer(undefined, {})).toEqual(expected)
+  })
+
+  it('Should Load movies to state', () => {
+    const expected = cleanData;
+    expect(moviesReducer(undefined, actions.loadMovies(cleanData))).toEqual(expected)
+  })
+})
