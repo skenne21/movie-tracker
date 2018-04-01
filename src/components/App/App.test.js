1c85b15b3dc857e930fff395d24e95f290958094
Sabrina Kennedy <skenne21@gmail.com>
2018-03-31 20:37:34 -0600

    Add movie tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enyzme';
 
-it.skip('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () =>{
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+  })
+
+  it.skip('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('Should match the snapshot', () =>{
+    expect(wrapper).toMatchSnapshot()
+  })
+
+})
+
+
+
 
 
