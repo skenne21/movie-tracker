47726f53816194c80aaad94a29af3c562ca83607
Sabrina Kennedy <skenne21@gmail.com>
2018-04-01 18:40:34 -0600

    Add App tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,22 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
+import { cleanData } from '../../mocks/mockMovieData';
 
 describe('App', () => {
-  let wrapper;
+  let wrapper, mockFetchMovies;
 
   beforeEach(() => {
-    wrapper = shallow(<App/>)
-  })
-
-  it.skip('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    mockFetchMovies = jest.fn()
+    wrapper = shallow(
+      <App fetchMovies={mockFetchMovies}/>, {disableLifecycleMethods: true});
   });
 
-  
-
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
 })
 
 
